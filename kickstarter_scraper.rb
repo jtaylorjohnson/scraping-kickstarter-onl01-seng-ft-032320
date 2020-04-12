1d1ecb4cc0c583ba3a6f55db11ba8404af0b98ce
James Johnson <jtaylorjohnson@gmail.com>
2020-04-12 19:38:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,6 +14,9 @@ def create_project_hash
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
   
@@ -21,11 +24,9 @@ def create_project_hash
   
 end
 
-create_project_hash
-
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-location: project.css("span.location-name").text
-percentL project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i 
+# location: project.css("span.location-name").text
+# percentL project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i 
