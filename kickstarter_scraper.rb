5f5a2400c6a018f190917aa4e9a52b713d1dd36b
James Johnson <jtaylorjohnson@gmail.com>
2020-04-12 19:11:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,7 @@ require "pry"
 
 def create_project_hash
 
-  htlml = File.read('fixtures/kickstarter.html')
+  html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
     binding.pry
