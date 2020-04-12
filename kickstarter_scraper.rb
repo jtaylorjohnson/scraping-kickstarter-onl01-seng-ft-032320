d6d5a6ca3849f9b378f56170556f01bae53e2c09
James Johnson <jtaylorjohnson@gmail.com>
2020-04-12 19:37:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,18 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-    binding.pry
+   
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value
+    }
+  end
+  
+  projects
+  
 end
 
 create_project_hash
@@ -17,3 +28,4 @@ create_project_hash
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 location: project.css("span.location-name").text
+percentL project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i 
